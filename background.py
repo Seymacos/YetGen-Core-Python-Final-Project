e4f0b3babeb6043370aa72651b7c8c463c9241f9
Elif Öztosun <eoztosun12@gmail.com>
2024-08-09 14:49:01 +0300

    update (loops are clear now)

--- background.py
+++ background.py
@@ -42,27 +42,13 @@ class World():
                     img_rect.y= row_count * tile_size
                     tile= (img, img_rect)
                     self.tile_list.append(tile) 
-                col_count+=1
-            row_count+=1
-
-        row_count=0
-        for row in data:
-            col_count=0
-            for tile in row:
                 if tile==2:
                     img= pygame.transform.scale(grass_img, (tile_size, tile_size))
                     img_rect= img.get_rect()
                     img_rect.x= col_count* tile_size
                     img_rect.y= row_count * tile_size
                     tile= (img, img_rect)
-                    self.tile_list.append(tile) 
-                col_count+=1
-            row_count+=1
-
-        row_count=0
-        for row in data:
-            col_count=0
-            for tile in row:
+                    self.tile_list.append(tile)
                 if tile==7:
                     img= pygame.transform.scale(exit_img, (tile_size, tile_size*2))
                     img_rect= img.get_rect()
