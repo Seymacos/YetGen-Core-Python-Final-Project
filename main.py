e7572b1d3a26959e0c391b15480e6579e67dd942
Şeyma COŞTUR <seymacostur0@gmail.com>
2024-08-15 21:05:56 +0300

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from enemies import Snake1, Snake2, badMushrooms, goodMushrooms, Bird
 from marioDeneme import Mario
 from scrolling_background import World
-from menu import draw_menu, event_handler
+from menu import Menu
 
 # Pygame'i başlat
 pygame.init()
@@ -19,17 +19,16 @@ pygame.display.set_caption("************")
 LIGHT_BLUE = (173, 216, 230)
 
 # background images 
-sun= pygame.image.load("sun.png")
-backg= pygame.image.load("sky.png")
-backg_new= pygame.transform.scale(backg, (SCREEN_WIDTH, SCREEN_HEIGHT))
-
+sun = pygame.image.load("sun.png")
+backg = pygame.image.load("sky.png")
+backg_new = pygame.transform.scale(backg, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # ekranı bölüyoruz, parçaları yerleştirmeyi kolaylaştırmak için 12*20 (değişebilir)
-tile_size= 40 # mainde grid_screen func çağırılacak
-scroll_speed=3
-background_scroll= 0
+tile_size = 40 # mainde grid_screen func çağırılacak
+scroll_speed = 3
+background_scroll = 0
 
-#world
+
 world_data = [
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -50,61 +49,75 @@ world_data = [
     [2,2,2,2,2,1,1,2,2,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,2,2,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ]
-
-world= World(world_data)
+world = World(world_data)
 
 # Mario karakterini başlat
 mario = Mario(SCREEN_WIDTH, SCREEN_HEIGHT)
 
- # Düşmanları başlat 
+# Düşmanları başlat 
 snake1 = Snake1(y=200, width=100, height=100, image_path="snake.png", speed=5, screen_width=SCREEN_WIDTH, new_width=100, new_height=100)
 snake2 = Snake2(y=300, width=100, height=100, image_path="snake2.png", speed=2, screen_width=SCREEN_WIDTH, new_width=100, new_height=100)
-bird = Bird(x=200,y=400, width=100, height=100, image_path="Bird.png", speed=2,  new_width=100, new_height=100)
-badMushrooms1=badMushrooms(x=100,y=20,image_path="BadMushrooms.png",new_width=100, new_height=100)
-Mushrooms1=goodMushrooms(x=150,y=100,image_path="Mushrooms.png",new_width=100, new_height=100)
-
+bird = Bird(x=200, y=400, width=100, height=100, image_path="Bird.png", speed=2, new_width=100, new_height=100)
+badMushrooms1 = badMushrooms(x=100, y=20, image_path="BadMushrooms.png", new_width=100, new_height=100)
+Mushrooms1 = goodMushrooms(x=150, y=100, image_path="Mushrooms.png", new_width=100, new_height=100)
 
 # Düşmanları güncelle
 all_sprites = pygame.sprite.Group()
-all_sprites.add(snake1,snake2,bird,badMushrooms1,Mushrooms1)
+all_sprites.add(snake1, snake2, bird, badMushrooms1, Mushrooms1)
+
+# Menü
+menu = Menu(SCREEN_WIDTH, SCREEN_HEIGHT)
+in_menu = True
 
 # Oyun döngüsü
 clock = pygame.time.Clock()
 
-#main_menu= True 
-#menu_items = ["Start", "Options", "Quit"]
-#selected_item = 0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    keys = pygame.key.get_pressed()
-    
-    #ekranı böl ve arka planı koy
-    screen.blit(backg_new, (0,0))
-    
-    world.draw(screen, background_scroll)
+    if in_menu:
+        # Menü ekranını çiz
+        menu.draw_menu(screen)
 
-    # Mario'nun hareketini yönet
-    mario.update(world) 
+        # Menü olaylarını yönet
+        action = menu.handle_events()
+        if action == "start":
+            in_menu = False
+        elif action == "options":
+            # Option ekranı burada işlenebilir
+            pass
+        elif action == "quit":
+            pygame.quit()
+            sys.exit()
+    else:
+        keys = pygame.key.get_pressed()
+
+        # ekranı böl ve arka planı koy
+        screen.blit(backg_new, (0, 0))
     
-    all_sprites.update()
+        world.draw(screen, background_scroll)
 
-   # Mario'yu ekrana çiz
-    mario.draw(screen)
+        # Mario'nun hareketini yönet
+        mario.update(world) 
+    
+        all_sprites.update()
 
-    # Düşmanları ekrana çiz
-    all_sprites.draw(screen)
+        # Mario'yu ekrana çiz
+        mario.draw(screen)
+
+        # Düşmanları ekrana çiz
+        all_sprites.draw(screen)
                 
-    # update the scroll position to make it continuous
-    background_scroll+= scroll_speed
-    if background_scroll>= SCREEN_WIDTH:
-        background_scroll= 0
+        # update the scroll position to make it continuous
+        background_scroll += scroll_speed
+        if background_scroll >= SCREEN_WIDTH:
+            background_scroll = 0
 
-    pygame.display.update()
+        pygame.display.update()
 
-                # Ekranı güncelle
+    # Ekranı güncelle
     pygame.display.flip()
     clock.tick(60)
--- menu.py
+++ menu.py
@@ -1,82 +1,48 @@
 import pygame
 import sys
 
-# Pygame'i başlat
-pygame.init()
-
-# Ekran boyutları
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Mario Game - Main Menu")
-
-# Renkler
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-SKY_BLUE = (135, 206, 235)
-
-# Yazı tipi ayarları
-font_path = r"SuperMario256.ttf"
-font = pygame.font.Font(font_path, 74)
-small_font = pygame.font.Font(font_path, 50)
-
-# Menü seçenekleri
-menu_items = ["Start", "Options", "Quit"]
-selected_item = 0
-
-# Arka plan resmi
-background_path = r"mario_background.png"
-background = pygame.image.load(background_path)
-background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
-
-def draw_menu():
-    screen.fill(SKY_BLUE)
-    screen.blit(background, (0, 0))
-    for i, item in enumerate(menu_items):
-        if i == selected_item:
-            label = font.render(item, True, BLACK)
-        else:
-            label = small_font.render(item, True, BLACK)
+class Menu:
+    def __init__(self, screen_width, screen_height):
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.menu_items = ["Start", "Options", "Quit"]
+        self.selected_item = 0
+        self.font_path = "SuperMario256.ttf"
+        self.font = pygame.font.Font(self.font_path, 74)
+        self.small_font = pygame.font.Font(self.font_path, 50)
+        self.background = pygame.image.load("mario_background.png")
+        self.background = pygame.transform.scale(self.background, (screen_width, screen_height))
         
-        label_rect = label.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + i * 100))
-        screen.blit(label, label_rect)
-
-    pygame.display.flip()
-
-main_menu= True
-
-def event_handler():
-    global selected_item
-    global main_menu
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
-                selected_item = (selected_item + 1) % len(menu_items)
-            elif event.key == pygame.K_UP:
-                selected_item = (selected_item - 1) % len(menu_items)
-            elif event.key == pygame.K_RETURN:
-                if menu_items[selected_item] == "Start":
-                    print("Starting game...")
-                    main_menu=False
-                elif menu_items[selected_item] == "Options":
-                    print("Opening options...")
-                elif menu_items[selected_item] == "Quit":
-                    pygame.quit()
-                    sys.exit()
-
-def main():
-    global selected_item
-    while True:
+    def draw_menu(self, screen):
+        screen.fill((135, 206, 235))
+        screen.blit(self.background, (0, 0))
+        for i, item in enumerate(self.menu_items):
+            if i == self.selected_item:
+                label = self.font.render(item, True, (0, 0, 0))
+            else:
+                label = self.small_font.render(item, True, (0, 0, 0))
+            
+            label_rect = label.get_rect(center=(self.screen_width // 2, self.screen_height // 2 + i * 100))
+            screen.blit(label, label_rect)
         
+        pygame.display.flip()
+
+    def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        draw_menu()
-        event_handler()        
-
-if __name__ == "__main__":
-    main()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    self.selected_item = (self.selected_item + 1) % len(self.menu_items)
+                elif event.key == pygame.K_UP:
+                    self.selected_item = (self.selected_item - 1) % len(self.menu_items)
+                elif event.key == pygame.K_RETURN:
+                    if self.menu_items[self.selected_item] == "Start":
+                        return "start"
+                    elif self.menu_items[self.selected_item] == "Options":
+                        return "options"
+                    elif self.menu_items[self.selected_item] == "Quit":
+                        pygame.quit()
+                        sys.exit()
+        return "menu"
