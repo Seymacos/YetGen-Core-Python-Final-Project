e8d5e109b25c98cdfca2b338193fa1d31625bc4a
Zeynep Sude Çetintaş <zeynepsudecetintas@hotmail.com>
2024-08-16 22:36:36 +0300

    karakterleri çalıştırmak için

---
+++ mainDosyaKrakterDenemesi.py
@@ -0,0 +1,140 @@
+import pygame
+import sys
+from enemies import Snake1, Snake2, badMushrooms, goodMushrooms, Bird
+from marioDeneme import Mario
+from background_new import World
+from menu import Menu
+
+# Pygame'i başlat
+pygame.init()
+
+# Ekran boyutları
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+game_over = 0
+max_levels = 3
+level = 1
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("************")
+
+# Renkler
+LIGHT_BLUE = (173, 216, 230)
+BLACK = (0, 0, 0)
+
+# background images
+sun = pygame.image.load("sun.png")
+backg = pygame.image.load("sky.png")
+backg_new = pygame.transform.scale(backg, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+# ekranı bölüyoruz, parçaları yerleştirmeyi kolaylaştırmak için 12*20 (değişebilir)
+tile_size = 40 # mainde grid_screen func çağırılacak
+
+world_data = [
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,2,2,2,0,0,0,0,0,0,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,1,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,2,2,1,0,0,0,0,0,6,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,2,2,2,2,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,0,0,0,0,0,0,0,0,2,0,0,1],
+    [7,0,0,0,0,0,0,0,0,2,2,2,2,0,0,0,0,0,0,0,0,0,2,2,2,2,0,0,0,0,0,1],
+    [0,0,0,6,0,2,2,0,0,1,1,1,1,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,2,2,2,2,1,1,2,2,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+]
+
+
+# Dünya nesnesini oluştur
+world = World(world_data)
+
+# Mario karakterini başlat
+mario = Mario(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+# Menü
+menu = Menu(SCREEN_WIDTH, SCREEN_HEIGHT)
+in_menu = True
+
+# Oyun döngüsü
+clock = pygame.time.Clock()
+
+'''def draw_game_over():
+    font = pygame.font.Font(None, 74)
+    game_over_text = font.render('Oyun Bitti!', True, BLACK)
+    text_rect = game_over_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+    screen.blit(game_over_text, text_rect)
+'''
+def draw_game_over(screen):
+    font = pygame.font.Font(None, 74)  # Büyük bir yazı tipi oluştur
+    text = font.render('Oyun Bitti!', True, (255, 0, 0))  # Kırmızı renkte yazı
+    text_rect = text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+    screen.blit(text, text_rect)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    if in_menu:
+        # Menü ekranını çiz
+        menu.draw_menu(screen)
+
+        # Menü olaylarını yönet
+        action = menu.handle_events()
+        if action == "start":
+            in_menu = False
+        elif action == "options":
+            # Option ekranı burada işlenebilir
+            pass
+        elif action == "quit":
+            pygame.quit()
+            sys.exit()
+    else:
+        keys = pygame.key.get_pressed()
+
+        # Ekranı böl ve arka planı koy
+        screen.blit(backg_new, (0, 0))
+
+        # Mario'nun hareketini yönet
+        mario.update(world, world.all_sprites_group)
+
+        # Ekranı güncelle
+        world.draw(screen)
+        mario.draw(screen)
+        world.all_sprites_group.update()
+        world.all_sprites_group.draw(screen)
+
+        if not mario.is_alive:
+            if not game_over:
+                game_over = True
+                game_over_start_time = pygame.time.get_ticks()  # Şu anki zamanı al
+
+            # Mario öldüyse, oyunun bitişini işleyin
+            screen.fill(LIGHT_BLUE)  # Ekranı arka plan rengiyle doldur
+            draw_game_over(screen)
+            pygame.display.flip()
+
+            # 3 saniye boyunca bekle
+            current_time = pygame.time.get_ticks()
+            if current_time - game_over_start_time >= 3000:  # 3000 ms = 3 saniye
+                # Başlangıç ekranına dön
+                in_menu = True
+                game_over = False
+                mario = Mario(SCREEN_WIDTH, SCREEN_HEIGHT)  # Mario'yu yeniden başlat
+                world = World(world_data)  # Dünya nesnesini yeniden oluştur
+        else:
+            game_over = False
+
+    # Ekranı güncelle
+    pygame.display.flip()
+    clock.tick(60)      
